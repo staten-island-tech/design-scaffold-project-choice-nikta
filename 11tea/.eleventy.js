9ac95a7b792a26eecb4465326021c472e88b94cb
NikitaYarin <90780642+NikitaYarin@users.noreply.github.com>
2022-04-13 10:55:12 -0400

    started netifly

--- 11tea/.eleventy.js
+++ 11tea/.eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/style");
   eleventyConfig.addPassthroughCopy("src/md");
   eleventyConfig.addPassthroughCopy("src/media");
+  eleventyConfig.addPassthroughCopy("src/admin");
   return {
     dir: {
       input: "src",
